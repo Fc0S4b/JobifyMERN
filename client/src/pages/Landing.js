925d20eb51d3f7a354c4d9f3455cec9b3007afc1
Fc0S4b <fcoj.sabugo@gmail.com>
2022-09-11 17:22:25 -0300

    Landing Page - Structure

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,30 @@
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
 
 const Landing = () => {
   return (
-    <h4>Landing page</h4>
-  )
-}
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" class="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+            quos nihil eveniet voluptatibus. Dolore eos cupiditate, itaque quos
+            vitae ad quam consequuntur quibusdam nostrum dolorum recusandae
+            facilis neque, commodi corrupti.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
+};
 
-export default Landing
+export default Landing;
