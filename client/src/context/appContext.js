9448c1d057f57218c16927d974b20ec55a5840d5
Fc0S4b <fcoj.sabugo@gmail.com>
2022-09-16 16:29:25 -0300

    Register User - Complete

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useContext } from 'react';
 
 import reducer from './reducer';
+import axios from 'axios';
 
 import {
   DISPLAY_ALERT,
@@ -18,6 +19,7 @@ const initialState = {
   user: null,
   token: null,
   userLocation: '',
+  jobLocation: '',
 };
 
 const AppContext = React.createContext();
@@ -36,9 +38,29 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
-  const registerUser = async (currentUser) =>{
-    console.log(currentUser)
-  }
+  const registerUser = async (currentUser) => {
+    dispatch({ type: REGISTER_USER_BEGIN });
+    try {
+      const response = await axios.post('/api/v1/auth/register', currentUser);
+      // const data = await response.json();
+      console.log(response);
+      const { user, token, location } = response.data;
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: { user, token, location },
+      });
+      // local storage later
+    } catch (error) {
+      console.log(error.response);
+      dispatch({
+        type: REGISTER_USER_ERROR,
+        payload: {
+          msg: error.response.data.msg,
+        },
+      });
+    }
+    clearAlert();
+  };
 
   return (
     <AppContext.Provider value={{ ...state, displayAlert, registerUser }}>
