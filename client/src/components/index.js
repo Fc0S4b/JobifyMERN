677fead3d1f7241f70ddfede47384a84353a0bb2
Fc0S4b <fcoj.sabugo@gmail.com>
2022-09-17 23:42:26 -0300

    Structure - Shared Layout

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,7 @@
 import Alert from './Alert';
+import BigSidebar from './BigSidebar';
 import FormRow from './FormRow';
 import Logo from './Logo';
-export { Logo, FormRow, Alert };
+import Navbar from './Navbar';
+import SmallSidebar from './SmallSidebar';
+export { Logo, FormRow, Alert, Navbar, BigSidebar, SmallSidebar };
